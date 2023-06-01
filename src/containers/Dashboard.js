ebc2df76a0962487de72b501223fe0d9ac22b01f
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-06-02 00:14:50 +0200

    Fix Dashboard sections

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,27 +131,30 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
+    if (this.counter === undefined || this.index !== index) this.counter = 0;
+    if (this.index === undefined || this.index !== index) this.index = index;
     if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' });
+      const filtered = filteredBills(bills, getStatus(this.index));
+      $(`#status-bills-container${this.index}`).html(cards(filtered));
+  
+      // Attach event handlers only to the tickets in the current list
+      filtered.forEach((bill) => {
+        $(`#open-bill${bill.id}`).click((e) =>
+          this.handleEditTicket(e, bill, bills)
+        );
+      });
+  
+      this.counter++;
     } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' });
+      $(`#status-bills-container${this.index}`).html('');
+      this.counter++;
     }
-
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
+  
+    return bills;
   }
+  
 
   getBillsAllUsers = () => {
     if (this.store) {
