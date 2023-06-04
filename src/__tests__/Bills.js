d186e3ff8bdcc6774bd318f945592e22d7ef36c6
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-06-04 11:46:52 +0200

    Add missing import

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,6 +3,7 @@
  */
 
 import {screen, waitFor} from "@testing-library/dom"
+import '@testing-library/jest-dom/extend-expect';
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
